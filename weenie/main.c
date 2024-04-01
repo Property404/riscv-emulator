43d007ed49bd3da6f9407ab729282d56a1aa6567
Property404 <git@dagans.dev>
2024-04-01 16:57:55 -0500

    Weenie: fizzbuzz

--- weenie/main.c
+++ weenie/main.c
@@ -5,7 +5,15 @@
 #include <stdio.h>
 
 int main() {
-    for (int i=0; i<4; i++) {
-        printf("%x: Hello, world!\n", i);
+    for (int i=0; i<=15; i++) {
+        if ( i % 3 == 0 && i % 5 != 0 ) {
+            printf("Fizz\n");
+        } else if ( i % 5 == 0 && i % 3 != 0 ) {
+            printf("Buzz\n");
+        } else if ( i % 3 == 0 && i % 5 == 0 ) {
+            printf("FizzBuzz\n");
+        } else {
+            printf("%x\n", i);
+        }
     }
 }
